f7fdf815062f40597fb475c1f537623a63f73d7a
Ailsadm <ailsamcgowan@hotmail.co.uk>
2023-01-29 15:32:37 +0000

    fivedayforecast started

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,36 @@
 let APIKey = "b467884d90d5f5312a326152e875f308";
 let weatherReportDetails = document.querySelector("#weatherReportDetails");
 let weatherReport = "";
-let longitude = ""; 
-// result.city.coord.lat
-let latitude = ""
-// result.city.coord.lon
+let longitude = "";
+let latitude = "";
+let fiveDayForecast = document.querySelector("#fiveDayForecast")
 
-// displayWeatherReportDetails()
 
 function coordinates (){
     let queryURL =  "http://api.openweathermap.org/geo/1.0/direct?q=" + $("#search-input").val() + "&appid=b467884d90d5f5312a326152e875f308"
     fetch(queryURL)
     .then(response => response.json())
     .then(function (result) {
-        // console.log(result)
         longitude = result[0].lon;
         latitude = result[0].lat
-        displayWeatherReportDetails()
+        displayWeatherReportDetails();
+        displayfiveDayForecast();
     });
 
 }
 
  function  displayWeatherReportDetails(){
 
-    // coordinates();
-    // console.log(longitude)
     let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=b467884d90d5f5312a326152e875f308";
     
 
     fetch(queryURL)
     .then(response => response.json())
     .then(function (result) {
-        console.log(result)
+        // console.log(result)
+        // $("#date").text(moment().format("DDD MMM, YYYY"))
 
-        $("#date").text(moment().format("DDD MMM, YYYY"))
-
-        weatherReportDetails.innerHTML = `<h1>${result.city.name}</h1>
+        weatherReportDetails.innerHTML = `<h1>${result.city.name}</h1> <p>${result.list[0].dt_txt}
         <p>${result.list[0].main.temp}</p>
         <p>${result.list[0].main.humidity}</p>
         <p>${result.list[0].wind.speed}</p>
@@ -46,11 +41,39 @@ function coordinates (){
 
 }
 
+function displayfiveDayForecast(){
+
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=b467884d90d5f5312a326152e875f308";
+    
+
+    fetch(queryURL)
+    .then(response => response.json())
+    .then(function (result) {
+
+        let weatherArray = [result.list]
+        // console.log(result.list)
+
+        for (let i = 0; i < weatherArray.length; i = i+7) {
+            const weatherDay = weatherArray[i];
+
+            console.log(weatherArray[i])
+
+            // fiveDayForecast.innerHTML = `<p>${result.list[3].clouds.dt_txt}</p>
+            // <p>${weatherDay.main.temp}</p>
+            // <p>${weatherDay.main.humidity}
+            // <img src="${'https://openweathermap.org/img/w/' + weatherDay.weather[0].icon + '.png'}"`
+            
+        }
+
+})
+};
+
 document.querySelector("#search-button").addEventListener("click", function(event){
     event.preventDefault();
 
     coordinates()
 
+    // displayfiveDayForecast()
+
     // displayWeatherReportDetails()
 })
-
